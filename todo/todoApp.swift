b801baaa80ec4dfdfe5a8bec50015891cb377268
Evgenii Govorushkin <e.govorushkin@gmail.com>
2026-07-01 13:32:03 +0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Evgenii Govorushkin on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
